93b96908ac44ca29b1f753544e6cb86d760b9408
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-06 22:21:43 +0300

    python

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 '''second task'''
-import math
 
 
 def to_str(n: float) -> str:
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''second task'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''concat func'''
+    return sum(input_list)
