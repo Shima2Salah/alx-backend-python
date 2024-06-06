65a2ec3b789f0e61d535a52d1f08ab9ca721fcd8
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-07 01:11:38 +0300

    python

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,10 +4,9 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    '''zoom array func'''
     zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
