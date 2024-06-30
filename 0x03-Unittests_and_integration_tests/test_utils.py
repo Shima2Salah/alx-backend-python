5bbfe8683196bd76a7a023d9821495cdef940f07
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-30 16:34:55 +0300

    mysql_data

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 '''test parameters'''
 import unittest
-from  utils import access_nested_map
+from  utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest import mock
+from unittest.mock import patch, Mock
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -19,6 +22,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+    ("http://example.com", {"payload": True}),
+    ("http://holberton.io", {"payload": False})])
+    @mock.patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value.json.return_value = test_payload  
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
