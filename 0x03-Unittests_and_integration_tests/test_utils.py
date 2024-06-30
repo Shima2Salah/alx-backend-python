424edefef0d51a18ef78efaa7ef158fdce96af4a
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-30 19:00:51 +0300

    mysql_data

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 '''test parameters'''
 import unittest
-from  utils import access_nested_map, get_json, memoize
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 from unittest import mock
 from unittest.mock import patch
 
 
-
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1), 
+        ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)])
     def test_access_nested_map(self, nested_map, path, expected):
@@ -23,16 +22,18 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     @parameterized.expand([
-    ("http://example.com", {"payload": True}),
-    ("http://holberton.io", {"payload": False})])
+                ("http://example.com", {"payload": True}),
+                ("http://holberton.io", {"payload": False})])
     @mock.patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
-        mock_get.return_value.json.return_value = test_payload  
+        mock_get.return_value.json.return_value = test_payload
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
 
 class TestMemoize(unittest.TestCase):
     def test_memoize(self):
@@ -49,6 +50,7 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(instance.a_property, 42)
             self.assertEqual(instance.a_property, 42)
             memo_func.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
