85679a6031bf56d8438e55f353c594d623d425e2
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-30 22:47:04 +0300

    mysql_data

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ from client import GithubOrgClient
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
 from unittest import mock
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, MagicMock, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -16,6 +16,15 @@ class TestGithubOrgClient(unittest.TestCase):
         instance = GithubOrgClient(org_name)
         instance.org()
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+
+def test_public_repos_url():
+    """Test that the result of _public_repos_url is the expected one based on the mocked payload"""
+    with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+        mock_org.return_value = {'repos_url': 'https://api.github.com/orgs/google/repos'}
+        instance = GithubOrgClient('google')
+        assert instance._public_repos_url == 'https://api.github.com/orgs/google/repos'
+
 
 if __name__ == '__main__':
     unittest.main()
