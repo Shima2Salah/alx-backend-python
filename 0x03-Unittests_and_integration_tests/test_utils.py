0b746999b09f650ab321a4996825e56713a48815
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-30 19:20:27 +0300

    mysql_data

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,46 +1,25 @@
 #!/usr/bin/env python3
-"""A module for testing the utils module.
-"""
+'''test parameters'''
 import unittest
-from typing import Dict, Tuple, Union
-from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-
-from utils import (
-    access_nested_map,
-    get_json,
-    memoize,
-)
+from unittest import mock
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
-    def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
-        """Tests `access_nested_map`'s output."""
+        ({"a": {"b": 2}}, ("a", "b"), 2)])
+    def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
-    def test_access_nested_map_exception(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            exception: Exception,
-            ) -> None:
-        """Tests `access_nested_map`'s exception raising."""
-        with self.assertRaises(exception):
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
 
@@ -74,3 +53,7 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(instance.a_property, 42)
             self.assertEqual(instance.a_property, 42)
             memo_func.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
