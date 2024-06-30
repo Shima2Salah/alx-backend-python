b86172027765026d75af21e4c4c0b3ee7b803720
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-30 23:36:42 +0300

    mysql_data

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -19,25 +19,38 @@ class TestGithubOrgClient(unittest.TestCase):
 
 
     def test_public_repos_url(self):
-        """Test that the result of _public_repos_url is the expected one based on the mocked payload"""
-        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
-            payload = {'repos_url': 'https://api.github.com'}
-            mock_org.return_value = payload
-            instance = GithubOrgClient('google')
-            self.assertEqual(instance._public_repos_url, payload["repos_url"])
+        """ Test that the result of _public_repos_url is the expected one
+        based on the mocked payload
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
 
     @patch('client.get_json')
-    def test_public_repos(self, mock_get_json):
-        payload = [{"name": "repo1"}, {"name": "repo2"}]
-        mock_get_json.return_value = payload
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock_org:
-            mock_org.return_value = "https://api.github.com/orgs/google/repos"
-            instance = GithubOrgClient('google')
-            result = instance.public_repos()
-            check = [i["name"] for i in payload]
+    def test_public_repos(self, mock_json):
+        """
+        Test that the list of repos is what you expect from the chosen payload.
+        Test that the mocked property and the mocked get_json was called once.
+        """
+        json_payload = [{"name": "Google"}, {"name": "Twitter"}]
+        mock_json.return_value = json_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public:
+
+            mock_public.return_value = "hello/world"
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+
+            check = [i["name"] for i in json_payload]
             self.assertEqual(result, check)
-            mock_get_json.assert_called_once()
-            mock_org.assert_called_once()
+
+            mock_public.assert_called_once()
+            mock_json.assert_called_once()
 
 
 if __name__ == '__main__':
